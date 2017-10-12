a499bdf9a1cf58989a4855e49bced0d5d08b312d
tomwey <tomwey@163.com>
2017-10-12 18:16:11 +0800

    ccc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { CatalogPage } from '../pages/catalog/catalog';
+import { PodcastPage } from '../pages/podcast/podcast';
+import { SearchPage } from '../pages/search/search';
+import { SettingPage } from '../pages/setting/setting';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +17,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    CatalogPage,
+    PodcastPage,
+    SearchPage,
+    SettingPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +31,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    CatalogPage,
+    PodcastPage,
+    SearchPage,
+    SettingPage,
     TabsPage
   ],
   providers: [
